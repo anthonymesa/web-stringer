2541943f66a95eecbca691a864a7710b66983244
Anthony Mesa <admesa@pm.me>
2024-06-18 22:13:21 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,165 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import {
+    Button,
+    Card,
+    Center,
+    createTheme,
+    Group,
+    MantineProvider,
+    Space,
+    Stack,
+    Text,
+    TextInput,
+} from "@mantine/core";
+import { peer, useAppStore } from "./Store";
+import QRCode from "react-qr-code";
+
+const theme = createTheme({});
+
+function SenderButton() {
+    const setRole = useAppStore((store) => store.setRole);
+    const handleOnCLick = () => setRole("sender");
+
+    return (
+        <Button
+            variant="default"
+            onClick={handleOnCLick}
+        >
+            Sender
+        </Button>
+    );
+}
+
+function RecieverButton() {
+    const setRole = useAppStore((store) => store.setRole);
+    const handleOnCLick = () => setRole("receiver");
+
+    return (
+        <Button
+            variant="default"
+            onClick={handleOnCLick}
+        >
+            Reciever
+        </Button>
+    );
+}
+
+function ClearRoleButton() {
+    const setRole = useAppStore((store) => store.setRole);
+    const closeConnection = useAppStore((store) => store.closeConnection);
+    const setReceiverId = useAppStore((store) => store.setReceiverId);
+    const clearDataLog = useAppStore((store) => store.clearDataLog);
+
+    const handleOnCLick = () => {
+        setRole("");
+        closeConnection();
+        setReceiverId("");
+        clearDataLog();
+    };
+
+    return (
+        <Button
+            variant="default"
+            onClick={handleOnCLick}
+        >
+            X
+        </Button>
+    );
+}
+
+function CardHeader() {
+    const role = useAppStore((store) => store.role);
+
+    return (
+        <Group justify="space-between">
+            <Text>{role}</Text>
+            <ClearRoleButton />
+        </Group>
+    );
+}
+
+function SenderCard() {
+    const connection = useAppStore((store) => store.connection);
+    const openConnection = useAppStore((store) => store.openConnection);
+    const setReceiverId = useAppStore((store) => store.setReceiverId);
+    const receiverId = useAppStore((store) => store.receiverId);
+
+    return (
+        <>
+            <TextInput
+                placeholder="Sender ID..."
+                value={receiverId}
+                onChange={(e) => setReceiverId(e.currentTarget.value)}
+            />
+            <Space h="md" />
+            {!connection && (
+                <Button
+                    disabled={receiverId == ""}
+                    onClick={openConnection}
+                >
+                    Connect
+                </Button>
+            )}
+        </>
+    );
+}
+
+function ReceiverCard() {
+    const beginReceiving = useAppStore((store) => store.beginReceiving);
+    const dataLog = useAppStore((store) => store.dataLog);
+    const receiving = useAppStore((store) => store.receiving);
+
+    return (
+        <>
+            <Text>ID: {peer.id}</Text>
+            <QRCode value={peer.id} />
+            <Space h="md" />
+            {!receiving && (
+                <Button onClick={beginReceiving}>Start Receiving...</Button>
+            )}
+            <Stack>
+                {dataLog.map((each) => (
+                    <Text key={each}>{each}</Text>
+                ))}
+            </Stack>
+        </>
+    );
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const role = useAppStore((store) => store.role);
+
+    const RoleInterface = () =>
+        role == "sender" ? <SenderCard /> : <ReceiverCard />;
+
+    return (
+        <MantineProvider theme={theme}>
+            <div className="App">
+                {role == "" ? (
+                    <Stack
+                        h={"100%"}
+                        justify="center"
+                        align="center"
+                    >
+                        <SenderButton />
+                        <RecieverButton />
+                    </Stack>
+                ) : (
+                    <Center
+                        h="100%"
+                        w="100%"
+                    >
+                        <Card w="75%">
+                            <CardHeader />
+                            <Space h="md" />
+                            <RoleInterface />
+                        </Card>
+                    </Center>
+                )}
+            </div>
+        </MantineProvider>
+    );
 }
 
 export default App;
---
+++ src/Store.js
@@ -0,0 +1,72 @@
+import { create } from "zustand";
+import assert from "assert";
+import Peer from "peerjs";
+import timestamp from "time-stamp";
+
+export const peer = new Peer();
+
+export const useAppStore = create((set) => ({
+    role: "",
+    receiverId: "",
+    senderId: "",
+    receiving: false,
+    connection: null,
+    dataLog: [],
+    setRole: (value) => {
+        assert(
+            ["sender", "receiver", ""].includes(value),
+            "Store.useAppStore.setRole(value): Unexpected value."
+        );
+        set({ role: value });
+    },
+    openConnection: () => {
+        set((state) => {
+            assert(
+                !state.connection,
+                "Store.openConnection: Connection exists."
+            );
+
+            const conn = peer.connect(state.receiverId);
+
+            conn.on("open", () => {
+                conn.send("Hello!");
+            });
+            conn.on("data", (data) => {
+                console.log(data);
+            });
+
+            return { connection: conn };
+        });
+    },
+    closeConnection: () => {
+        set({ connection: null });
+    },
+    setReceiverId: (value) => {
+        assert(
+            typeof value != String,
+            "Store.setReveiverId: Id must be string"
+        );
+        set({ receiverId: value });
+    },
+    beginReceiving: () => {
+        peer.on("connection", (conn) => {
+            conn.on("data", (data) => {
+                set((state) => ({
+                    dataLog: [
+                        ...state.dataLog,
+                        `${timestamp("YYYY/MM/DD HH:mm:ss")} - ${
+                            conn.peer
+                        } - ${data}`,
+                    ],
+                }));
+            });
+            conn.on("open", () => {
+                console.log("Connected successfully");
+            });
+        });
+        set({ receiving: true });
+    },
+    clearDataLog: () => {
+        set({ dataLog: [] });
+    },
+}));
